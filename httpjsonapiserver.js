3661d4f045c3728a9f4e8bc1b1ce0a8e0a61eb73
nelsonic <nodecoder@gmail.com>
2014-03-25 08:20:43 +0000

    tidy up. still passes

--- httpjsonapiserver.js
+++ httpjsonapiserver.js
@@ -6,17 +6,17 @@ server.on('request', function(req, res) {
 	if(req.method !== 'GET') {
 		return res.end('Sorry, we only accept GET requests');
 	}
-	// console.log(req.url);
+
 	var path = req.url.split('?')[0],
-		ds = req.url.split('=')[1];
+		  ds = req.url.split('=')[1];
 
 	res.writeHead(200, { 'Content-Type': 'application/json' })
 	if(path === '/api/parsetime'){
-		res.end( JSON.stringify(dateFromTimeString(ds)) )
+		res.end( JSON.stringify(dateFromTimeString(ds)) );
 	}
 	else if(path === '/api/unixtime') {
 		var uts = { "unixtime": new Date(Date.parse(ds)).getTime() }
-		res.end( JSON.stringify(uts)  )
+		res.end( JSON.stringify(uts) );
 	}
 });
 
