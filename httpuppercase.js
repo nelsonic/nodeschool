0facbbb66861e43639bc065fbda3eebadb6ad09f
nelsonic <nodecoder@gmail.com>
2014-03-25 06:36:41 +0000

    adds check for POST method

--- httpuppercase.js
+++ httpuppercase.js
@@ -3,6 +3,9 @@ var http = require('http');
 var server = http.createServer().listen(Number(process.argv[2]))
 
 server.on('request', function(req, res) {
+	if(req.method !== 'POST') {
+		return res.end('Only Accept Post');
+	}
 	var str = ''
 	req.on('data', function(chunk){
 		str = str + chunk;
