1e31b10d90ffe536a5e3dbc765f42d7a156759c4
nelsonic <nodecoder@gmail.com>
2014-04-20 19:48:25 +0100

    clarifies buffer prameter

--- stream-adventure/html-stream.js
+++ stream-adventure/html-stream.js
@@ -2,8 +2,8 @@ var trumpet = require('trumpet')();
 var through = require('through');
 
 var loud = trumpet.select('.loud').createStream();
-loud.pipe(through(function (buf) {
-    this.queue(buf.toString().toUpperCase());
+loud.pipe(through(function (buffer) {
+    this.queue(buffer.toString().toUpperCase());
 })).pipe(loud);
 
 process.stdin.pipe(trumpet).pipe(process.stdout);
