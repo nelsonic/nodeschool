b5c37bd32ef460bcb44902aec3d6019dbf600deb
nelsonic <nodecoder@gmail.com>
2014-05-01 12:53:23 +0100

    passes Basics BATCH

--- levelmeup/05-Basics-BATCH.js
+++ levelmeup/05-Basics-BATCH.js
@@ -1,9 +1,8 @@
-var level = require('level');
 var fs = require('fs');
+var level = require('level');
 
-var db = level(process.argv[2]);
+// read file and split into an array of lines
 var file = fs.readFileSync(process.argv[3], 'utf8');
-
 var lines = file.split('\n');
 
 level(process.argv[2], function (err, db) {
@@ -11,11 +10,11 @@ level(process.argv[2], function (err, db) {
   var batch = db.batch();
   lines.forEach(function (line) {
     var d = line.split(',');
-    if (d[0] == 'del'){
-      batch.del(d[1]);
+    if (d[0] == 'del') {
+      return batch.del(d[1]);
     } else {
       batch.put(d[1], d[2]);
     }
   });
-  datch.write();
+  batch.write();
 });
