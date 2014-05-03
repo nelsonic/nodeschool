335d7af4313a3781e305a8c912726ac469c8fcfd
nelsonic <nodecoder@gmail.com>
2014-05-03 17:54:46 +0100

    what the hell is \xff and why do I need it?!

--- levelmeup/08-@horse_js-Tweets.js
+++ levelmeup/08-@horse_js-Tweets.js
@@ -1,6 +1,6 @@
 module.exports = function (db, day, callback) {
   var tweets = []
-  db.createReadStream({ start: day, end: day})
+  db.createReadStream({ start: day, end: day  + '\xff' })
     .on('data', function (data) {
       tweets.push(data.value);
     })
