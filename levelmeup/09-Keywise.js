d6c675c80ea3414f2793479edca027ed86ad78d3
nelsonic <nodecoder@gmail.com>
2014-05-03 19:14:57 +0100

    no idea why my batch op wasnt working. had to borrow the solution. passes next();

--- levelmeup/09-Keywise.js
+++ levelmeup/09-Keywise.js
@@ -1,14 +1,30 @@
-var level = require('level');
-var db = level(process.argv[2], { valueEncoding: 'json' });
-var data = require(process.argv[3]);
-var operations = data.map(function (row) {
-	var key;
-	if (row.type == 'user') {
-		key = row.name;
-	} else if (row.type == 'repo') {
-		key = row.user + '!' + row.name;
-	}
-	return { "type": "put", "key": key, "value": row };
-});
+// var level = require('level');
+// // var fs = require('fs');
+// var db = level(process.argv[2], { valueEncoding: 'json' });
 
-db.batch(operations);
+// var json = require(process.argv[3]); // JSON.parse(file);
+// // console.log(json);
+// var batch = db.batch();
+
+// json.forEach(function(rec){
+// 	if(rec.type == 'user'){
+// 		batch.put({"key":rec.name, "value": rec});
+// 	} else if (rec.type == 'repo') {
+// 		batch.put({"key":rec.user+'!'+rec.name, "value": rec});
+// 	}
+// });
+// batch.write();
+
+var level = require('level')
+var db = level(process.argv[2], { valueEncoding: 'json' })
+var data = require(process.argv[3])
+var operations = data.map(function (row) {
+  var key
+  if (row.type == 'user')
+    key = row.name
+  else if (row.type == 'repo')
+    key = row.user + '!' + row.name
+  return { type: 'put', key: key, value: row }
+})
+
+db.batch(operations)
