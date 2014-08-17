2601a5ee4a85908696885709a590b9b1fc09dc5e
nelsonic <contact.nelsonic@gmail.com>
2014-08-17 23:34:01 +0100

    adds recursive solution

--- functional-javascript/02-Higher-Order-Functions.js
+++ functional-javascript/02-Higher-Order-Functions.js
@@ -3,5 +3,13 @@ function repeat(operation, num) {
 		operation();
 	}
 }
+
+// official solution:
+// function repeat(operation, num) {
+// 	if (num <= 0) return;
+// 	operation();
+// 	return repeat(operation, --num);
+// }
+
 // Do not remove the line below
 module.exports = repeat;
