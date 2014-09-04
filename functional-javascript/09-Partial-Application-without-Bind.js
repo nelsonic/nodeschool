88cda0ad6c4f4a01d471c05060601120395035a8
nelsonic <contact.nelsonic@gmail.com>
2014-09-05 00:22:46 +0100

    partial application :-?

---
+++ functional-javascript/09-Partial-Application-without-Bind.js
@@ -0,0 +1,10 @@
+var slice = Array.prototype.slice;
+
+function logger(namespace) {
+  return function() {
+    console.log.apply( console,
+      [namespace].concat(slice.call(arguments)) );
+  }
+}
+
+module.exports = logger;
